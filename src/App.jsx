44054e4e8c26a9c2111170be7bd9b2e1924fc3bf
J Huertas <huertasfam@gmail.com>
2026-03-13 04:10:56 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Onboarding from './pages/Onboarding';
 import Settings from './pages/Settings';
 import AdminAvatarProcessor from './pages/AdminAvatarProcessor';
 import AdminDashboard from './pages/AdminDashboard';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 // Add page imports here
 
 const AuthenticatedApp = () => {
