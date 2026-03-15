ea129837bddcf9dd468a18a09236502f894ead39
J Huertas <huertasfam@gmail.com>
2026-03-15 22:11:50 +0000

    File changes

--- src/pages/VibePage.jsx
+++ src/pages/VibePage.jsx
@@ -67,7 +67,7 @@ export default function VibePage() {
 
   return (
     <AppShell
-      tabs={false}
+      tabs={true}
       bg="linear-gradient(180deg, #06020f 0%, #120626 40%, #1a0535 100%)"
     >
       {/* Ambient glow */}
